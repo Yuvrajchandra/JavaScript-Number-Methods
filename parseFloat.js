627d1534e521c82f780e46d7967859f9a360caf9
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-05 23:44:01 +0530

    parseFloat

---
+++ parseFloat.js
@@ -0,0 +1,17 @@
+let num1 = Number.parseFloat("34.235");
+console.log(num1);
+
+let num2 = Number.parseFloat(" 5324.45    ");
+console.log(num2);
+
+let num3 = Number.parseFloat("32.65");
+console.log(num3);
+
+let num4 = Number.parseFloat("2 Welcome MUO");
+console.log(num4);
+
+let num5 = Number.parseFloat("Welcome 2 MUO");
+console.log(num5);
+
+let num6 = Number.parseFloat("#$^$^");
+console.log(num6);
