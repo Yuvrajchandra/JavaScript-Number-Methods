00b6999747e696be6f265936e36b724912f4b407
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-05 23:45:34 +0530

    toPrecision

---
+++ toPrecision.js
@@ -0,0 +1,14 @@
+let num1 = 234.345;
+console.log(num1.toPrecision(4));
+
+let num2 = -783.234;
+console.log(num2.toPrecision(5));
+
+let num3 = 213;
+console.log(num3.toPrecision(4));
+
+let num4 = 345.23;
+console.log(num4.toPrecision(3));
+
+let num5 = 785.123;
+console.log(num5.toPrecision(5));
