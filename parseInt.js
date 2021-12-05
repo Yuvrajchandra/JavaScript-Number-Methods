92c463e695004cf6136b10f88dbad14e96e516d6
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-05 23:44:17 +0530

    parseInt

---
+++ parseInt.js
@@ -0,0 +1,14 @@
+let num1 = Number.parseInt("34");
+console.log(num1);
+
+let num2 = Number.parseInt("5324    ");
+console.log(num2);
+
+let num3 = Number.parseInt("32.65");
+console.log(num3);
+
+let num4 = Number.parseInt("Hello, World!");
+console.log(num4);
+
+let num5 = Number.parseInt("...#@$$");
+console.log(num5);
