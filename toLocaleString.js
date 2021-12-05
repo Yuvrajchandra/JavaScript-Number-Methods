2c552c70b64903f54f82d14983cecd6e0a993bf8
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-05 23:45:17 +0530

    toLocaleString

---
+++ toLocaleString.js
@@ -0,0 +1,10 @@
+let num = 762359.237;
+
+// Indian
+console.log(num.toLocaleString('en-IN'));
+
+// Chinese
+console.log(num.toLocaleString('zh-Hans-CN-u-nu-hanidec'));
+
+// German
+console.log(num.toLocaleString('de-DE'));
