f5525a1bac0ac5ffa39a6bf4f0eb4599cd5ddb57
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-05 23:44:38 +0530

    toExponential

---
+++ toExponential.js
@@ -0,0 +1,14 @@
+let num1 = 23425;
+console.log(num1.toExponential());
+
+let num2 = 342;
+console.log(num2.toExponential(2));
+
+let num3 = 465500;
+console.log(num3.toExponential(4));
+
+let num4 = 886.456;
+console.log(num4.toExponential());
+
+let num5 = 0.34;
+console.log(num5.toExponential());
