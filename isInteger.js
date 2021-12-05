e563d7df6a745e71a44686c09f57bf1e084d3846
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-05 23:43:07 +0530

    isInteger

---
+++ isInteger.js
@@ -0,0 +1,29 @@
+let num1 = 45;
+console.log(Number.isInteger(num1));
+
+let num2 = 0;
+console.log(Number.isInteger(num2));
+
+let num3 = 1;
+console.log(Number.isInteger(num3));
+
+let num4 = 0.8;
+console.log(Number.isInteger(num4));
+
+let num5 = 8.0;
+console.log(Number.isInteger(num5));
+
+let num6 = Infinity;
+console.log(Number.isInteger(num6));
+
+let num7 = NaN;
+console.log(Number.isInteger(num7));
+
+let num8 = [1, 2, 3];
+console.log(Number.isInteger(num8));
+
+let num9 = "45";
+console.log(Number.isInteger(num9));
+
+let num10 = true;
+console.log(Number.isInteger(num10));
