a124c463430194329b2fe1442ea7da94b408542c
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-05 23:44:58 +0530

    toFixed

---
+++ toFixed.js
@@ -0,0 +1,14 @@
+let num1 = 234.345;
+console.log(num1.toFixed(1));
+
+let num2 = -783.234;
+console.log(num2.toFixed(2));
+
+let num3 = 213;
+console.log(num3.toFixed(4));
+
+let num4 = 345.23;
+console.log(num4.toFixed());
+
+let num5 = 785.123;
+console.log(num5.toFixed(0));
