fd4dcf1051a6eb7f29029ff2192af55350cd3bae
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-05 23:45:48 +0530

    toString

---
+++ toString.js
@@ -0,0 +1,12 @@
+let num1 = 213;
+console.log(num1.toString());
+
+let num2 = 25.56;
+console.log(num2.toString());
+
+let num3 = -673;
+console.log(num3.toString());
+
+let num4 = 15;
+// Base 2
+console.log(num4.toString(2));
