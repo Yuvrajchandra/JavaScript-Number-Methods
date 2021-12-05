05c4e59c5564ac9ba864f1a6410f41dae9751b1d
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-05 23:46:03 +0530

    valueOf

---
+++ valueOf.js
@@ -0,0 +1,11 @@
+let num1 = 234.345;
+console.log(num1.valueOf());
+
+let num2 = -783.234;
+console.log(num2.valueOf());
+
+console.log((327).valueOf());
+
+console.log((25+25).valueOf());
+
+console.log((0.003).valueOf());
